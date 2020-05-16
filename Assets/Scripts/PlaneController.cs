ed5f4799dd685c4184358e9a118bb0c2513ce4aa
Cthulhu <elvin.bayramov@protonmail.com>
2020-05-16 20:27:20 +0400

    Created plane basic controller and basics of simulator controller

---
+++ Assets/Scripts/PlaneController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlaneController : MonoBehaviour
+{
+    public RectTransform trajectoryLine;
+    public PlanePosition planePositions;
+    public float mSpeed, mHeightSpeed;
+    public float mDesiredHeight;
+    private float mCurrentHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = planePositions.source;
+        mCurrentHeight = mDesiredHeight;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = Time.deltaTime * mSpeed;
+        transform.position = Vector3.MoveTowards(transform.position, planePositions.destination, step);
+        
+        float neededDHeight = mDesiredHeight - mCurrentHeight;
+        float speedH = Mathf.Sign(neededDHeight) * mHeightSpeed * Time.deltaTime;
+        if (Mathf.Abs(neededDHeight) > Mathf.Abs(speedH)) {
+            mCurrentHeight += speedH;
+        } else {
+            mCurrentHeight += neededDHeight;
+        }
+        
+        trajectoryLine.rotation =  Quaternion.LookRotation(Vector3.RotateTowards(trajectoryLine.forward, planePositions.destination, step, 0.0f));
+        trajectoryLine.localScale = new Vector3(3.0f, 3.0f, Mathf.Min((planePositions.destination - transform.position).magnitude / 2.0f, 3.0f));
+    }
+    
+}
+
+
+[Serializable]
+public struct PlanePosition {
+    public Vector3 source;
+    public Vector3 destination;
+}
+
---
+++ Assets/Scripts/SimulatorController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulatorController : MonoBehaviour
+{
+    public Camera viewCamera;
+    public GameObject planePrefab;
+    
+    public PlanePosition[] planes;
+
+    public int planesCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        foreach (var planePosition in planes) {
+            var plane = Instantiate(planePrefab, planePosition.source, Quaternion.identity);
+            var c = plane.GetComponent<PlaneController>();
+            c.planePositions = planePosition;
+            c.mSpeed = Random.Range(1.0f, 3.0f);
+        }
+
+
+        Debug.Log(viewCamera.orthographicSize);
+
+        for (int i = 0; i < planesCount; i++) {
+            PlanePosition position = new PlanePosition {
+                source = new Vector3(Random.Range(-20.0f, 20.0f), Random.Range(-10.0f, 10.0f)),
+                destination = new Vector3(Random.Range(-20.0f, 20.0f), Random.Range(-10.0f, 10.0f))
+            };
+            float speed = Random.Range(1.0f, 2.0f);
+            var plane = Instantiate(planePrefab, Vector3.zero, Quaternion.identity);
+            var controller = plane.GetComponent<PlaneController>();
+            controller.planePositions = position;
+            controller.mSpeed = speed;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
